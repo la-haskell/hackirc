d57008afc081a3ff8a54ca078f9f168d8d462a72
Phil Freeman <paf31@cantab.net>
2013-11-14 19:45:26 -0800

    Basic socket server

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -16,5 +16,24 @@ module Main (
     main
 ) where
 
+import Network.Socket
+import Control.Concurrent
+
 main :: IO ()
-main = putStrLn "todo"
+main = do
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bindSocket sock (SockAddrInet 1801 iNADDR_ANY)
+  listen sock 1000
+  mainLoop sock
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+  conn <- accept sock
+  forkIO (runConn conn)
+  mainLoop sock
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+  send sock "Hi!\n"
+  sClose sock
