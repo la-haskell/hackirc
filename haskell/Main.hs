6ce1164d9bc716876bd85799c1a0694fd55bb6f9
Phil Freeman <paf31@cantab.net>
2013-11-14 20:26:09 -0800

    more progress

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -16,6 +16,7 @@ module Main (
     main
 ) where
 
+import Control.Monad.State
 import Network.Socket
 import Control.Concurrent
 import Control.Monad
@@ -23,13 +24,35 @@ import Data.IORef
 
 import qualified Data.Map as M
 
+type Room = String
+
+type User = String
+
+type Msg = String
+
 data ServerState = ServerState
-  { users :: M.Map String Socket
-  , rooms :: M.Map String [String]
-  }
+  { users :: M.Map User Socket
+  , rooms :: M.Map Room [User]
+  } deriving (Show)
+
+data UserLocalState = UserLocalState
+  { myUserName :: User
+  , myRooms :: [Room]
+  } deriving (Show)
 
 defaultState :: ServerState
 defaultState = ServerState M.empty M.empty
+
+emptyUserState :: User -> UserLocalState
+emptyUserState = flip UserLocalState []
+
+handle :: IORef ServerState -> [String] -> StateT UserLocalState IO String
+handle state ["LOGOUT"] = logout state
+handle state ["JOIN", room] = joinRoom state room
+handle state ["LEAVE", room] = leave state room
+handle state ("SAY" : room : msg) = say state room (unwords msg)
+handle state ("WHISPER" : user : msg) = whisper state user (unwords msg)
+handle _ _ = return ("ERROR Unknown command")
 
 main :: IO ()
 main = do
@@ -40,11 +63,40 @@ main = do
   listen sock 1000
   forever $ do
     conn <- accept sock
-    forkIO $ forever $ do
-      cmd <- recv sock 1024
-      resp <- handle state cmd
-      send sock resp
+    forkIO $ userLoop state sock
 
-handle :: IORef ServerState -> String -> IO String
-handle _ = return
+userLoop :: IORef ServerState -> Socket -> IO ()
+userLoop serverState sock = do
+  loginCmd <- recv sock 1024
+  case words loginCmd of
+    ["LOGIN", username] -> do
+      login serverState username
+      go (emptyUserState username)
+    _ -> do
+      send sock "ERROR Inappropriate command"
+      sClose sock
+  where
+  go :: UserLocalState -> IO ()
+  go userState = do
+    cmd <- recv sock 1024
+    (resp, newUserState) <- flip runStateT userState $ handle serverState $ words cmd
+    send sock resp
+    go newUserState
 
+login :: IORef ServerState -> User -> IO String
+login = undefined
+
+logout :: IORef ServerState -> StateT UserLocalState IO String
+logout = undefined
+
+joinRoom :: IORef ServerState -> Room -> StateT UserLocalState IO String
+joinRoom = undefined
+
+leave :: IORef ServerState -> Room -> StateT UserLocalState IO String
+leave = undefined
+
+say :: IORef ServerState -> Room -> Msg -> StateT UserLocalState IO String
+say = undefined
+
+whisper :: IORef ServerState -> Room -> Msg -> StateT UserLocalState IO String
+whisper = undefined
