28ea370ad271c98f6ec8c07ee9d0d5dfc426be30
Phil Freeman <paf31@cantab.net>
2013-11-14 20:05:08 -0800

    IORef

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -19,25 +19,32 @@ module Main (
 import Network.Socket
 import Control.Concurrent
 import Control.Monad
+import Data.IORef
+
+import qualified Data.Map as M
+
+data ServerState = ServerState
+  { users :: M.Map String Socket
+  , rooms :: M.Map String [String]
+  }
+
+defaultState :: ServerState
+defaultState = ServerState M.empty M.empty
 
 main :: IO ()
 main = do
+  state <- newIORef defaultState
   sock <- socket AF_INET Stream 0
   setSocketOption sock ReuseAddr 1
   bindSocket sock (SockAddrInet 1801 iNADDR_ANY)
   listen sock 1000
-  mainLoop sock
+  forever $ do
+    conn <- accept sock
+    forkIO $ forever $ do
+      cmd <- recv sock 1024
+      resp <- handle state cmd
+      send sock resp
 
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
-  conn <- accept sock
-  forkIO (runConn conn)
-  mainLoop sock
+handle :: IORef ServerState -> String -> IO String
+handle _ = return
 
-handle :: [String] -> [String]
-handle = id
-
-runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, _) = forever $ do
-  cmd <- recv sock 1024
-  send sock $ unwords . handle . words $ cmd
