2e569e8fa4c4311b5f028daaf2f7704b031fd2fe
Phil Freeman <paf31@cantab.net>
2013-11-14 19:41:39 -0800

    Main.hs

---
+++ haskell/Main.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :
+-- License     :  MIT
+--
+-- Maintainer  :  lahaskell@elem.com
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Main (
+    main
+) where
+
+main :: IO ()
+main = putStrLn "todo"
